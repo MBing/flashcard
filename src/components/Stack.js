37a2449d62c6cb5c408c912de3450690b4ed7820
Martin Bing <bing.martin.photography@gmail.com>
2019-05-01 12:27:31 +0200

    Refactor some Components to stateless comps

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Card } from './Card';
 
-export class StackContainer extends Component {
-    render() {
-        const { title, cards } = this.props.stack;
-
-        return (
-            <div>
-                <Link to="/" className="link-home">
-                    <h4>Home</h4>
-                </Link>
-                <h3>{title}</h3>
-                {cards.map(card => (
-                    <Card card={card} key={card.id} />
-                ))}
-            </div>
-        );
-    }
-}
+export const StackContainer = ({ stack: { title, cards } }) => (
+    <div>
+        <Link to="/" className="link-home">
+            <h4>Home</h4>
+        </Link>
+        <h3>{title}</h3>
+        {cards.map(card => (
+            <Card card={card} key={card.id} />
+        ))}
+    </div>
+);
 
 const mapStateToProps = state => ({ stack: state.stack });
 
